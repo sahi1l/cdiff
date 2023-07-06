dfba8a87ad3357f07bf829f6c956f40b7c588a52
Sam Hill <physhill@gmail.com>
2023-07-06 14:22:50 -0400

    Found a newer version of cdiff.py that works a lot better

--- cdiff.py
+++ cdiff.py
@@ -1,48 +1,96 @@
 #!/usr/bin/env python3
-"""Usage: cdiff dir1 dir2"""
-import os,sys
-from termcolor import colored
-exclude = [".ds_store",".saves",'.trash']
-def inonly(A,B):
-    lowerB = list(map(lambda x:x.lower(),B))
-    return [a for a in A if a.lower() not in lowerB and a.lower() not in exclude]
-def inboth(A,B):
-    lowerB = list(map(lambda x:x.lower(),B))
-    return [a for a in A if a.lower() in lowerB and a.lower() not in exclude]
+"""Usage: cdiff dir1 dir2
+Colors and simplifies the output of diff"""
+import sys
+import re
+import os
+from subprocess import Popen,PIPE
+import glob
+from termcolor import colored as col
+dir1 = sys.argv[1]
+dir2 = sys.argv[2]
+dir1 = glob.glob(dir1)[0].rstrip("/")
+dir2 = glob.glob(dir2)[0].rstrip("/")
+color1 = "red"
+color2 = "cyan"
+mode = ""
+binary_color="grey"
+drawline = "="*80
+result = []
+def prnt(*args):
+    global result
+    result += [' '.join(args)]
+def mode(txt,color="white"):
+    return col(txt,color,attrs=["bold"])
 
-def diff(rootA, rootB, dirname, lvl=0):
-    """given two folders, return:
-    - a list of files and directories which are in A but not B
-    - a list of files and directories which are in B but not A
-    - (a list of files in both that are different somehow)
-    - a list of directories which are in both (for later perusal, or perhaps recursiveness now?)
-    """
-    _,dirsA,filesA = next(os.walk(os.path.join(rootA,dirname)))
-    _,dirsB,filesB = next(os.walk(os.path.join(rootB,dirname)))
-    onlyA = inonly(dirsA,dirsB) + inonly(filesA,filesB)
-    onlyB = inonly(dirsB,dirsA) + inonly(filesB,filesA)
-    onlyA = []
-#    onlyA = [a for a in dirsA if a not in dirsB and a not in exclude] + [a for a in filesA if a not in filesB and a not in exclude]
-#    onlyB = [a for a in dirsB if a not in dirsA and a not in exclude] + [a for a in filesB if a not in filesA and a not in exclude]
-    bothdirs = inboth(dirsA,dirsB)
-    #[a for a in dirsA if a in dirsB and a not in exclude]
-    if not (onlyA or onlyB or bothdirs):
-        return False
-    if (onlyA or onlyB):
-        print(colored(dirname,"white",attrs=["underline"])+"/") #, len(onlyA),len(onlyB),len(bothdirs))
-    for x in sorted(onlyA):
-        attr = ["underline"] if x in dirsA else []
-        print(colored("A: "+os.path.basename(x),"red",attrs=attr))
-    for x in sorted(onlyB):
-        attr = ["underline"] if x in dirsB else []
-        print(colored("B: "+os.path.basename(x),"cyan",attrs=attr))
-    for x in sorted(bothdirs):
-        diff(rootA, rootB, os.path.join(dirname,x), lvl+1)
-    return True
-#    return (onlyA, onlyB, bothdirs)
+def hilite_path(path):
+    dir = os.path.dirname(path)
+    base = os.path.basename(path)
+    error = ""
+    color = "white"
+    if dir.startswith(dir1):
+        dir=dir.replace(dir1,"A")
+        color = color1
+    elif dir.startswith(dir2):
+        dir=dir.replace(dir2,"B")
+        color = color2
+    else:
+        print(f"{dir=},{dir1=},{dir2=}")
+        dir = "?"
+    return col(dir+"/",color)+col(base,color,attrs=["bold","underline"])
+                
+#stream = Popen(["diff","-r",dir1,dir2],stdout=PIPE,stderr=PIPE).communicate()[0]
+#stream = os.popen(f'diff -r "{dir1}" "{dir2}"')
+stream = Popen(f'diff -r "{dir1}" "{dir2}"',shell=True,stdout=PIPE).stdout
+print("OK got it")
+for line in stream:
+    #    try:
+#    line=line.decode().strip()
+    line=line.strip()
+#    except UnicodeDecodeError:
+#        print("error")
+#        continue
+    color=""
+    try:
+        line=line.decode()
+    except UnicodeDecodeError:
+        print(f"Error:{line}")
+        continue
+    if M:=re.match("Binary files (.*) and (.*) differ",line):
+        prnt(drawline)
+        prnt(mode("BINARY: "),
+              hilite_path(M.group(1)),
+              " and ",
+              hilite_path(M.group(2)))
+        
+    elif M:=re.match("Only in (.*): (.*)$",line):
+        prnt(drawline)
+        prnt(mode("ONLY IN: "),
+              hilite_path(M.group(1).rstrip("/")+"/"+M.group(2)))
 
+    elif M:=re.match(f"diff -r {dir1}/(.*) {dir2}/(.*)$",line):
+        prnt(drawline)
+        prnt(mode("DIFF: "),
+              col(M.group(1),color1),
+              col(M.group(2),color2))
+        
+    elif line.startswith("<"):
+        prnt(col(line[2:],color1))
+    elif line.startswith(">"):
+        prnt(col(line[2:],color2))
+    elif M:=re.match("([0-9,]*)d([0-9,]*)$",line):
+        prnt(mode("\nDELETE: ",color1),col(f"{M.group(1)} {M.group(2)}",color1))
+    elif M:=re.match("([0-9,]*)c([0-9,]*)$",line):
+        prnt(mode("\nCHANGE: ","magenta"),col(f"{M.group(1)} {M.group(2)}","magenta"))
+    elif M:=re.match("([0-9,]*)a([0-9,]*)$",line):
+        prnt(mode("\nADD: ",color2),col(f"{M.group(1)} {M.group(2)}",color2))
+        
+    else:
+        prnt(line)
 
-
-if __name__ == "__main__":
-    root = "." if len(sys.argv)<=3 else sys.argv[3]
-    diff(sys.argv[1],sys.argv[2],root)
+try:
+    for line in result:
+        print(line)
+except (BrokenPipeError,KeyboardInterrupt):
+    print("--")
+    exit
